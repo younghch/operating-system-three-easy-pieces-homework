fdf9472a740d17c0b2a5df76c2ba4fb7716f1c2c
Young <younghch42@gmail.com>
2022-05-12 11:27:10 +0900

    add temp

--- homework-code/virtualization/limited-direct-execution/test-on-linux.c
+++ homework-code/virtualization/limited-direct-execution/test-on-linux.c
@@ -1,95 +1,68 @@
 #define _GNU_SOURCE
-#define READ    0
-#define WRITE   1
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <sys/time.h>
 #include <sched.h>
 
-double get_elapsed_seconds(struct timeval start, struct timeval end)
-{
-    return end.tv_sec + end.tv_usec / 1000000.0 - start.tv_sec - start.tv_usec / 1000000.0;
-}
+int
+main(int argc, char *argv[]) {
+    // measure system call
+    int fd = open("./1.txt", O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU), nloops = 10;
 
-int measure_system_call(int number_of_loop)
-{
-    int count;
-    struct  timeval start, end;
-    
-    count = 0;
-    if (gettimeofday(&start, NULL) == -1)
-        return 1;
-    while (count < number_of_loop){
-        read(0, NULL, 0);
-        count++;
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+    for (size_t i = 0; i < nloops; i++) {
+        read(fd, NULL, 0);
     }
-    if (gettimeofday(&end, NULL)  == -1)
-        return 1;
-    printf("average time to execute %d syscall: %fs\n", number_of_loop, get_elapsed_seconds(start, end));
-    return 0;   
-}
+    gettimeofday(&end, NULL);
+    printf("system call: %f microseconds\n\n", (float) (end.tv_sec * 1000000 + end.tv_usec - start.tv_sec * 1000000 - start.tv_usec) / nloops);
+    close(fd);
 
-int measure_context_swtich(int number_of_loop)
-{
-    cpu_set_t       set;
-    int             count, p1;
-    unsigned int    cpu_id;
-    int             fd[2];
-    struct          timeval start, end;
-
+    // measure context switch
+    cpu_set_t set;
     CPU_ZERO(&set);
     CPU_SET(0, &set);
-    count = 0;
-    if 
-    (
-        pipe(fd) == -1 || 
-        (p1 = fork()) == -1 ||
-        sched_setaffinity(getpid(), sizeof(cpu_set_t), &set) == -1
-    ){
-        printf("something wrong here\n");
-        return 1;
+
+    int first_pipefd[2], second_pipefd[2];
+    if (pipe(first_pipefd) == -1) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
     }
-    printf("process : %d\n", p1);
-    if (p1 != 0)
-    {
-        if (gettimeofday(&start, NULL)  == -1)
-            return 1;
-        close(fd[READ]);
-        while (count < number_of_loop)
-        {
-            write(fd[WRITE], NULL, 0);
-            count += 2;
-            write(1, "parent write\n", 14);
+    if (pipe(second_pipefd) == -1) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+
+    pid_t cpid = fork();
+
+    if (cpid == -1) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    } else if (cpid == 0) {    // child
+        if (sched_setaffinity(getpid(), sizeof(cpu_set_t), &set) == -1) {
+            exit(EXIT_FAILURE);
         }
-        if (gettimeofday(&end, NULL) == -1 || getcpu(&cpu_id, NULL) == -1)
-            return 1;
-        printf("parent done on cpu %d\n", cpu_id);
-        printf("average time to execute %d context swtich: %fs\n", number_of_loop, get_elapsed_seconds(start, end));
-    }
-    else 
-    {
-        close(fd[WRITE]);
-        while (count < number_of_loop)
-        {
-            read(fd[READ], NULL, 0);     
-            count += 2;
+
+        for (size_t i = 0; i < nloops; i++) {
+            read(first_pipefd[0], NULL, 0);
             printf("child read\n");
+            write(second_pipefd[1], NULL, 0);
         }
-        if (getcpu(&cpu_id, NULL) == -1)
-            return 1;
-        printf("child done on cpu %d\n", cpu_id);
+    } else {           // parent
+        if (sched_setaffinity(getpid(), sizeof(cpu_set_t), &set) == -1) {
+            exit(EXIT_FAILURE);
+        }
+
+        gettimeofday(&start, NULL);
+        for (size_t i = 0; i < nloops; i++) {
+            write(first_pipefd[1], NULL, 0);
+            printf("parent read\n");
+            read(second_pipefd[0], NULL, 0);
+        }
+        gettimeofday(&end, NULL);
+        printf("context switch: %f microseconds\n", (float) (end.tv_sec * 1000000 + end.tv_usec - start.tv_sec * 1000000 - start.tv_usec) / nloops);
     }
-    return 0;
-}
-
-int main(int argc, char *argv[]) 
-{
-    int     number_of_loop;
-
-    if (argc == 1)  number_of_loop = 1000;
-    else            number_of_loop = atoi(argv[1]);
-    if (measure_system_call(number_of_loop))    return 1;
-    if (measure_context_swtich(number_of_loop)) return 1;
     return 0;
 }
