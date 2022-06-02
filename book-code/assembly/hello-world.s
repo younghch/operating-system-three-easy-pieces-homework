a749b5145a5f9603e1fa114db8c509cf9bb5a4ef
Young <younghch42@gmail.com>
2022-06-02 09:31:54 +0900

    studying assembly

---
+++ book-code/assembly/hello-world.s
@@ -0,0 +1,15 @@
+
+                segment .text
+                global  _start
+_start:         mov     edx, 13
+                mov     ecx, msg
+                mov     ebx, 1          ; stdout
+                mov     eax, 4          ; syscall - write
+                int     0x80            ; call kernel
+
+                mov     eax, 1          ; syscall - exit
+                int     0x80
+
+
+                segment .data
+msg             db      "Hello, World", 10
