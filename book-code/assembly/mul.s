9b86d9e7dc18f8fccd77550832bc386797bb4726
Young <younghch42@gmail.com>
2022-06-03 00:27:09 +0900

    Add mul

---
+++ book-code/assembly/mul.s
@@ -0,0 +1,44 @@
+;
+; A multiplies 3 with 2
+;
+;  difference between eax,ax,al,ah:
+;  - EAX is the full 32-bit value
+;  - AX is the lower 16-bits
+;  - AL is the lower 8 bits
+;  - AH is the bits 8 through 15 (zero-based)
+;
+
+SYS_EXIT    equ 1
+SYS_WRITE   equ 4
+STDIN       equ 0
+
+            segment .data
+
+msg         db      "The result is: ", 0xA,0xd
+len         equ     $ - msg
+
+            segment .bss
+res         resb    1
+
+            segment .text
+            global  _start
+_start:
+            mov     al, '3'
+            sub     al, '0'
+            mov     bl, '2'
+            sub     bl, '0'
+            mul     bl
+            add     al, '0'
+
+            mov     [res], al
+            mov     eax, SYS_WRITE
+            mov     ebx, STDOUT
+            mov     ecx, res
+            mov     edx, 1
+            int     0x80
+
+exit:
+            mov     eax, SYS_EXIT
+            xor     ebx, ebx
+            int     0x80
+            
