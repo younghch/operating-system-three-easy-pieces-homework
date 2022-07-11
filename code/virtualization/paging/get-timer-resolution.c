f89955cb9cf2bd5b7facda4a7b211ad0629d7e7a
Young <younghch42@gmail.com>
2022-07-11 22:45:48 +0900

    Fix the function of measuring the timer resolution, RTFM

--- code/virtualization/paging/get-timer-resolution.c
+++ code/virtualization/paging/get-timer-resolution.c
@@ -1,9 +1,10 @@
+#include <time.h>
 #include <stdio.h>
-#include "measure-time.h"
 
 int main(void)
 {
-    start_timer();
-    end_timer();
-    printf("elapsed time in seconds: %f\nelapsed time in nano seconds: %f\n", get_elapsed_seconds(), get_elapsed_nano_seconds());
+
+    struct  timespec    t;
+    clock_getres(CLOCK_PROCESS_CPUTIME_ID, &t);
+    printf("Reolution of the clock: %ld nsec\n", t.tv_nsec);
 }
