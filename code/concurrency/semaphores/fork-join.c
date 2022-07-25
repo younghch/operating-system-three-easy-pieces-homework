4b448ccaef122aa37dd7f5f37b0131f4e06fb30e
younghch <younghch@student.42seoul.kr>
2022-07-25 21:33:37 +0900

    done till q2, working on q3

--- code/concurrency/semaphores/fork-join.c
+++ code/concurrency/semaphores/fork-join.c
@@ -7,16 +7,16 @@ sem_t s;
 
 void *child(void *arg) {
     printf("child\n");
-    // use semaphore here
+    Sem_post(&s);
     return NULL;
 }
 
 int main(int argc, char *argv[]) {
     pthread_t p;
     printf("parent: begin\n");
-    // init semaphore here
+    Sem_init(&s, 0);
     Pthread_create(&p, NULL, child, NULL);
-    // use semaphore here
+    Sem_wait(&s);
     printf("parent: end\n");
     return 0;
 }
