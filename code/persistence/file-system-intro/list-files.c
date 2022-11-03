8cc37e63159013cb3da4d84fe42bda08d65657fc
Young <younghch42@gmail.com>
2022-11-03 20:17:01 +0900

    Add ls, fixing bug on ls-l

--- code/persistence/file-system-intro/list-files.c
+++ code/persistence/file-system-intro/list-files.c
@@ -12,7 +12,7 @@ int     main(int argc, char *argv[]) {
         path = ".";
     else if (argc == 2)
     {
-        if (strcmp(argv[1], "l") == 0)
+        if (strcmp(argv[1], "-l") == 0)
         {
             is_l = TRUE;
             path = ".";
@@ -23,17 +23,18 @@ int     main(int argc, char *argv[]) {
     else 
     {
         is_l = TRUE;
-        if (strcmp(argv[1], "l") == 0)
+        if (strcmp(argv[1], "-l") == 0)
             path = argv[2];
-        else if (strcmp(argv[2], "l") == 0)
+        else if (strcmp(argv[2], "-l") == 0)
             path = argv[1];
         else
             assert(TRUE == FALSE);
     }
 
     dir = opendir(path);
+    assert(dir != NULL);
 
-    if (ls_l == TRUE)
+    if (is_l == TRUE)
         ls_l(dir);
     else
         ls(dir);
@@ -42,25 +43,56 @@ int     main(int argc, char *argv[]) {
 
 }
 
-void    print_ls_stat(char *sub_path)
+void    print_ls(char *sub_path)
 {
+    if (strcmp(sub_path, ".") == 0 || strcmp(sub_path, "..") == 0)
+        return;
 
+    fputs(sub_path, stdout);
+    fputs("\t", stdout);
 }
 
-void    print_ls_l_stat(char *sub_path)
+void    print_ls_l(char *sub_path)
 {
+    struct  stat    st;
+    struct  passwd  *user;
+    struct  group   *group;
+    char            permission[10];
+    char            time_modify[12];
 
+    if (strcmp(sub_path, ".") == 0 || strcmp(sub_path, "..") == 0)
+        return;
+    
+    assert(stat(sub_path, &st) == 0);
+
+    user = getpwuid(st.st_uid);
+    group = getgrgid(st.st_gid);
+
+    set_permission(st.st_mode, permission);
+    set_time_format(&st.st_mtimespec, time_modify);
+    
+    puts(sub_path);
+    printf("%s\n", sub_path);
+    printf("%s\t%hu\t%s\t%s\t%5lld\t%s\n", permission, st.st_nlink, user->pw_name, group->gr_name, st.st_size, time_modify);
+    printf("%s\t%hu\t%s\t%s\t%5lld\t%s\t%s", permission, st.st_nlink, user->pw_name, group->gr_name, st.st_size, time_modify, sub_path);
+    
 }
 
 void    ls(DIR *dir) 
 {
+    struct dirent   *entry;
 
-
+    while ((entry = readdir(dir)) != NULL)
+        print_ls(entry->d_name);
+    puts("");
 }
 
 void    ls_l(DIR *dir) 
 {
+    struct dirent   *entry;
 
+    while ((entry = readdir(dir)) != NULL)
+        print_ls_l(entry->d_name);
 }
 
 void    set_permission(mode_t mode, char *permission)
@@ -85,10 +117,11 @@ void    set_permission(mode_t mode, char *permission)
     
 }
 
-void    set_time_format(struct timespec *ts, char *time_format) 
+void    set_time_format(struct timespec *ts, char  *time_format) 
 {
     struct  tm  tm;
 
     gmtime_r(&ts->tv_sec, &tm);
-    strftime(time_format, 12, "%m %d %H:%M", &tm);
+    strftime(time_format, 11, "%m %d %H:%M", &tm);
+    time_format[11] = 0;
 }
--- code/persistence/file-system-intro/list-files.h
+++ code/persistence/file-system-intro/list-files.h
@@ -3,13 +3,15 @@
 #include <stdio.h>
 #include <time.h>
 #include <string.h>
+#include <pwd.h>
+#include <grp.h>
 #include <assert.h>
 
 #define TRUE    1
 #define FALSE   0
 
-void    print_ls_stat(char *sub_path);
-void    print_ls_l_stat(char *sub_path);
+void    print_ls(char *sub_path);
+void    print_ls_l(char *sub_path);
 void    ls(DIR *dir);
 void    ls_l(DIR *dir);
 void    set_permission(mode_t mode, char *permission);
