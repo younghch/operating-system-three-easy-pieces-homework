4aabcc50fe98de8e2af428de8da45a3981a038d5
Young <younghch42@gmail.com>
2022-11-10 17:48:14 +0900

    Add header file, complete function

--- code/persistence/file-system-intro/tail.c
+++ code/persistence/file-system-intro/tail.c
@@ -1,10 +1,24 @@
-#include <unistd.h>
-#include <sys/stat.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
+#include "tail.h"
 
 int     g_result_length = 0;
+
+int     main(int argc, char *argv[]) {
+    int             lines, fd;
+    struct  stat    st;
+    blkcnt_t        block_index_backward;
+    char            *path;
+    char            **result;
+
+    parse_and_set_args(argc, argv, &lines, path);
+
+    assert(stat(path, &st) == 0);
+    assert((result = malloc(sizeof(char *)*st.st_blocks)) != NULL);
+    assert((fd = open(path, O_RDONLY)) != -1);
+    read_blocks_backward(&st, fd, lines, result);
+    print_result(result);
+    free_all(result);
+
+}
 
 void    parse_and_set_args(int argc, char *argv[], int *lines, char *path) {
     int     c;
@@ -13,7 +27,7 @@ void    parse_and_set_args(int argc, char *argv[], int *lines, char *path) {
         if ((c = getopt(argc, argv, "n:")) != -1) {
             switch (c) {
                 case 'n':
-                    lines = atoi(optarg);
+                    *lines = atoi(optarg);
                     break;
                 case '?':
                 default:
@@ -29,57 +43,76 @@ void    parse_and_set_args(int argc, char *argv[], int *lines, char *path) {
 void    read_blocks_backward(struct stat *st, int fd, int lines, char **result) {
     blksize_t   blk_size;
     off_t       offset;
-    int         lines_found;
+    int         new_line_chs;
     char        *buffer;
 
-    lines_found = 0;
+    new_line_chs = 0;
     blk_size = st->st_blksize;
     offset = (st->st_blocks-1)*blk_size;
 
+    // 5 sentences -> find 5 \n character -> if found 11 => read from 7th \n
     // TODO: check line count condition
-    while (lines_found < lines && offset >= 0) {
+    while (new_line_chs < lines && offset >= 0) {
         buffer = malloc(blk_size);
         lseek(fd, offset, SEEK_SET);
 
-        lines_found += check_lines_in_block(fd, buffer, blk_size);
+        new_line_chs += check_lines_in_block(fd, buffer, blk_size);
         
         result[g_result_length] = buffer;
         g_result_length++;
         offset -= blk_size;
     }
+
+    move_pointer_to_start(result[g_result_length-1], new_line_chs-lines+1);
     
 }
 
 int     check_lines_in_block(int fd, char *buffer, size_t blk_size) {
-    int count;
+    int     count;
+    int     i;
+    char    c;
 
     count = 0;
-    read(fd, buffer, blk_size);
-    // lines_found를 global로 변경하고 찾은 개수를 체크해 offset까지 변경해주는게 나을까?
+    i = 0;
 
+    read(fd, buffer, blk_size);
+    while ((c = buffer[i]) != 0){
+        if (c == '\n') count += 1;
+        i++;
+    }
     return count;
 }
 
-void    move_pointer_to_start(char* block) {
+void    move_pointer_to_start(char* block, int read_start) {
+    int     count;
+    int     i;
+    char    c;
 
+    count = 0;
+    i = 0;
+
+    while (count < read_start && (c = block[i]) != 0){
+        if (c =='\n') count += 1;
+        i++;
+    }
+    block += i;
+}
+
+void    print_result(char **result) {
+    int i;
+
+    i = g_result_length-1;
+    while (i >= 0){
+        puts(result[i]);
+    }
 }
 
 void    free_all(char **result) {
+    int i;
 
+    i = g_result_length-1;
+    while (i >= 0){
+        free(result[i]);
+    }
+    free(result);
 }
-
-int     main(int argc, char *argv[]) {
-    int             lines;
-    struct  stat    st;
-    blkcnt_t        block_index_backward;
-    char            *path;
-    char            **result;
-
-    parse_and_set_args(argc, argv, &lines, path);
-
-    assert(stat(path, &st) == 0);
-    assert((result = malloc(sizeof(char *)*st.st_blocks)) != NULL);
-    read_blocks_backward();
-
-}
-
